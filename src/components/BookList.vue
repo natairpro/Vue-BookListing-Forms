c0e09c2c0d4a998a27f7ef9b434dfdae4283150c
Nattress <cnattress@inntopia.com>
2018-12-27 18:03:49 -0700

    moving lodash

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -18,9 +18,10 @@
 </template>
 
 <script>
+import _ from "lodash";
 import BookItem from "./BookItem";
 import BookForm from "./BookForm";
-import _ from "lodash";
+
 
 export default {
   name: "BookList",
